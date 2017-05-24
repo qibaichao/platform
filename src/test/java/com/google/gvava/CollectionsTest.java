b627dac726e67c4786023ad0dcbd5cca55ead6ae
qibaichao <qibaichao@we.com>
2017-05-24 17:39:04 +0800

    btrace 整理

---
+++ src/test/java/com/google/gvava/CollectionsTest.java
@@ -0,0 +1,26 @@
+package com.google.gvava;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by qibaichao on 2016/6/30.
+ */
+public class CollectionsTest {
+
+    public static void main(String args[]){
+        Map<String, Map<Long, List<String>>> map = new HashMap<String, Map<Long,List<String>>>();
+
+        Map<String, Map<Long, List<String>>> map1 = Maps.newHashMap();
+
+        ImmutableList<String> of = ImmutableList.of("a", "b", "c", "d");
+
+        ImmutableMap<String,String> map2 = ImmutableMap.of("key1", "value1", "key2", "value2");
+
+    }
+}
